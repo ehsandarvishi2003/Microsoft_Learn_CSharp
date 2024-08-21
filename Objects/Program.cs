5f5868938715414ce98454c11af8bc5d703a18f1
Ehsan Darvishi <ehsandarvishi2003@gmail.com>
2024-08-21 11:56:31 +0330

    Add Inheritance

--- Objects/Program.cs
+++ Objects/Program.cs
@@ -28,6 +28,25 @@
 
             Console.WriteLine("person2 Name = {0} Age = {1}", person2.Name, person2.Age);
             Console.WriteLine("person1 Name = {0} Age = {1}", person1.Name, person1.Age);
+
+            //********************************************************************************************
+
+            // Create  struct instance and initialize by using "new".
+            // Memory is allocated on thread stack.
+            Person p1 = new Person("Alex", 9);
+            Console.WriteLine("p1 Name = {0} Age = {1}", p1.Name, p1.Age);
+
+            // Create  new struct object. Note that  struct can be initialized
+            // without using "new".
+            Person p2 = p1;
+
+            // Assign values to p2 members.
+            p2.Name = "Spencer";
+            p2.Age = 7;
+            Console.WriteLine("p2 Name = {0} Age = {1}", p2.Name, p2.Age);
+
+            // p1 values remain unchanged because p2 is  copy.
+            Console.WriteLine("p1 Name = {0} Age = {1}", p1.Name, p1.Age);
         }
     }
 }
@@ -38,3 +57,10 @@
     person2 Name = Molly Age = 16
     person1 Name = Molly Age = 16
 */
+
+/*
+       Output:
+       p1 Name = Alex Age = 9
+       p2 Name = Spencer Age = 7
+       p1 Name = Alex Age = 9
+   */
